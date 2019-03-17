4776342a462fad74185b564b08892159848895c3
Pratikshya Pandey <pratikshya.pp19@gmail.com>
2019-03-17 17:56:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,9 +1,11 @@
 def using_push(array,string)
   array.push(string)
 end
+
 def using_unshift(array,string)
   array.unshift(string)
 end
+
 def using_pop(array)
   array.pop
 end
@@ -13,12 +15,3 @@ def pop_with_args(array)
   array.pop(2)
 end
 
-def using_shift(array)
-  array.shift
-end
-  
-  def shift_with_args(array)
-    array.shift(2)
-  end
-  
-  
